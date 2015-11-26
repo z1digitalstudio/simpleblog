ea5232499648a7ce6142f25092bc1c7c6f626fc1
Alejandro Diaz <3balejandro@gmail.com>
2015-11-26 12:27:28 +0100

    Changed admin. Default User and forbid h1 When creating an entry the author will be the current user logged in. Forbid the use of h1 when the content is written.

--- admin.py
+++ admin.py
@@ -17,13 +17,15 @@ class EntryAdminForm(ModelForm):
 
     class Meta:
         widgets = {
+
             'content': RedactorEditor(redactor_settings={
+                'formattingTags': ['p', 'blockquote', 'pre', 'h2', 'h3',
+                                   'h4', 'h5', 'h6'],
                 'buttons': [
                     'formatting', 'bold', 'italic', 'deleted',
                     'unorderedlist', 'orderedlist', 'link', 'html'
-                ]
-            })
-        }
+                    ]}
+                )}
 
 
 class EntryAdmin(admin.ModelAdmin):
@@ -31,5 +33,12 @@ class EntryAdmin(admin.ModelAdmin):
     model = Entry
     prepopulated_fields = {'slug': ('title', )}
     exclude = ['publication_date']
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == 'author':
+            kwargs['initial'] = request.user.id
+        return super(EntryAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs
+        )
 
 admin.site.register(Entry, EntryAdmin)
