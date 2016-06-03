95bcb086f5c466b8fdf6f6eae9d2d9588a7554c6
Alejandro Diaz <3balejandro@gmail.com>
2016-06-03 14:45:56 +0200

    Minor change

--- simpleblog/context_processors.py
+++ simpleblog/context_processors.py
@@ -6,5 +6,5 @@ def simpleblog_context(request):
     entries = Entry.published_objects.all()[:settings.LAST_ENTRIES]
     categories = Category.objects.all()
     return {'categories': categories,
-            'entries': entries
+            'last_entries': entries
             }
