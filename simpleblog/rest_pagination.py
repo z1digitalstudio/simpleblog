485b8201f6e59c8dea5f3693519f7119383f23cf
dander94 <3balejandro@gmail.com>
2019-07-01 17:58:35 +0200

    Fix wrong attribute name

--- simpleblog/rest_pagination.py
+++ simpleblog/rest_pagination.py
@@ -2,4 +2,4 @@ from rest_framework.pagination import LimitOffsetPagination
 
 
 class LimitStartPagination(LimitOffsetPagination):
-    offset_query_parameter = 'start'
+    offset_query_param = 'start'
