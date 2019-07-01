5d65c62a229eae6ab1a14773cb1a4a8e8d866f58
dander94 <3balejandro@gmail.com>
2019-07-01 18:37:14 +0200

    Category Filter

--- simpleblog/rest_views.py
+++ simpleblog/rest_views.py
@@ -1,17 +1,20 @@
+from rest_framework import filters
 from rest_framework.generics import ListAPIView, RetrieveAPIView
 
 from simpleblog.models import Entry, Category
-from simpleblog.rest_filters import SearchCategoryFilter
 from simpleblog.rest_pagination import LimitStartPagination
 from simpleblog.serializers import CategorySerializer, EntrySerializer
 
 
+class CategoryFilter(filters.SearchFilter):
+    search_param = "category"
+
 class EntryListView(ListAPIView):
-    filter_backends = (SearchCategoryFilter, )
+    filter_backends = (CategoryFilter,)
     pagination_class = LimitStartPagination
     queryset = Entry.objects.all()
     serializer_class = EntrySerializer
-
+    search_fields = ('category__slug', )
 
 class EntryRetrieveView(RetrieveAPIView):
     queryset = Entry.objects.all()
